a70cb80c789751e986da4aa03e7c5f563d865c25
Solomon Gachure <gachuresolomon64@gmail.com>
2024-03-29 18:28:55 +0300

    Added analytics

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Contact from "./component/contact";
 import Navbar from "./component/navbar";
 import Projects from "./component/projects";
 import Skills from "./component/skills";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="dark:bg-stone-900">
-      <Navbar/>
-      <About/>
-      <Projects/>
-      <Skills/>
-      <Contact/>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Analytics />
     </div>
   );
 }
